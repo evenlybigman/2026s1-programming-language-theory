fa0fd2ba5867abbc57563838ee86e2e8fcccb1a9
Eve <evenlybigman@gmail.com>
2026-05-18 13:12:34 +0900

    참조에 의한 호출(배열)

---
+++ week11/array/funcA.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+int A(int x) {
+	printf("x = %d\n", x);
+ 	x = 20;
+	return x;
+}
+
+void B(int x[]) {
+	printf("x[0] = %d : x[1] = %d : x[2] = %d\n", x[0], x[1]);
+	x[0] = 100;
+	x[1] = 200;
+}
---
+++ week11/array/main.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+int A(int x);
+void B(int x[]);
+
+int main() {
+	int a = 10;
+	int b = 10;
+	
+	b = A(a);
+	printf("a = %d : b = %d\n", a, b);
+
+
+	int c[3];
+
+	c[0] = 10;
+	c[1] = 20;
+
+	B(c);
+	printf("c[0] = %d : c[1] = %d : c[2] = %d", c[0], c[1], c[2]);
+	
+	return 0;
+}
