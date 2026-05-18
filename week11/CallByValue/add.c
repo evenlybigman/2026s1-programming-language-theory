5294dedcf29a73644004f976adf940868e40bb9b
Eve <evenlybigman@gmail.com>
2026-05-18 12:44:05 +0900

    참조에 의한 호출 실습

--- week11/CallByValue/add.c
+++ week11/CallByValue/add.c
@@ -1,9 +1,16 @@
 ﻿#include <stdio.h>
 
 extern int big;
+extern int big2;
 
 void add(int a, int b) {
 	big += 10;
 	a += 2;
 	b += 4;
+}
+
+void padd(int* a, int* b) {
+	big2 += 10;
+	*a += 2;
+	*b += 4;
 }
