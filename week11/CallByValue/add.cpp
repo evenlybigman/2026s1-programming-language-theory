86ac4a773ae6fbb6b7d1f39b6297ec916b8966a5
Eve <evenlybigman@gmail.com>
2026-05-18 12:48:59 +0900

    참조에 의한 호출 &

--- week11/CallByValue/add.cpp
+++ week11/CallByValue/add.cpp
@@ -2,6 +2,7 @@
 
 extern int big;
 extern int big2;
+extern int big3;
 
 void add(int a, int b) {
 	big += 10;
@@ -13,4 +14,10 @@ void add2(int* a, int* b) {
 	big2 += 10;
 	*a += 2;
 	*b += 4;
+}
+
+void add3(int& a, int& b) {
+	big3 += 10;
+	a += 2;
+	b += 4;
 }
---
+++ week11/CallByValue/main.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+int big = 48;
+int big2 = 48;
+int big3 = 48;
+
+void add(int a, int b);
+void add2(int* a, int* b);
+void add3(int& a, int& b);
+
+int main() {
+	int i = 2;
+	int i2 = 2;
+	int i3 = 2;
+
+	add(i, big);
+	add2(&i2, &big2);
+	add3(i3, big3);
+	printf("i = %d : big = %d\n", i, big);
+	printf("i2 = %d : big2 = %d\n", i2, big2);
+	printf("i3 = %d : big3 = %d\n", i3, big3);
+
+	return 0;
+}
